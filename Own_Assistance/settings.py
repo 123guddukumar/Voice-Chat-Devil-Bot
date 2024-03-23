248b07b781439fe5b1ac8091a9a96a67cb713e50
DivyansuVerma <130806575+Divyansuverma@users.noreply.github.com>
2024-03-23 16:45:46 +0530

    Commit

--- Own_Assistance/settings.py
+++ Own_Assistance/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+OPENAI_API_KEY = os.environ.get('YourAPI')
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-wqo@sy!5jwe6juz12t_4ctqwe-ptqw_i8krye!)h71o!(_*$yu"
 
